d1ce89147440738594f0905350dc28fbbc115b43
Your Name <emilybuetow@Johns-MacBook-Pro-3.local>
2023-01-19 16:31:35 -0500

    created pwd.js file. Called pwd function in bash.js

--- bash.js
+++ bash.js
@@ -1,16 +1,1 @@
-//Output a prompt
-process.stdout.write('prompt > ');
-
-// The stdin 'data' event fires after a user types in a line
-process.stdin.on('data', (data) => {
-  // if what is passed in for the prompt is "pwd" then output the current file path
-    const cmd = data.toString().trim(); //remove the newline
-    if (cmd === "pwd"){
-        process.stdout.write(cwd);
-        // alternate way of saying the same code from above
-        // process.stdout.write( __dirname);
-    }
-    else process.stdout.write('Not a valid input.');
-//   process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
-});
+const exportsFrompwd = require('./pwd.js')
---
+++ pwd.js
@@ -0,0 +1,14 @@
+//Output a prompt
+process.stdout.write('prompt > ');
+module.exports =
+    process.stdin.on('data', (data) => {
+        // if what is passed in for the prompt is "pwd" then output the current file path
+          const cmd = data.toString().trim(); //remove the newline
+          if (cmd === "pwd"){
+              process.stdout.write(cwd);
+              // alternate way of saying the same code from above
+            //   process.stdout.write( __dirname);
+          }
+          else process.stdout.write('Not a valid input.');
+        process.stdout.write('\nprompt > ');
+      });
